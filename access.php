6729381d8dbaaed58182bfc6b6a8e82ed0ccb040
muhammedjailam <muhammedjailam@gmail.com>
2022-04-25 16:11:08 +0500

    - Added all php files from Moodle english language pack - Access.php translations added

---
+++ access.php
@@ -0,0 +1,44 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Strings for component 'access', language 'en', branch 'MOODLE_20_STABLE'
+ *
+ * @package   core_access
+ * @copyright 1999 onwards Martin Dougiamas  {@link http://moodle.com}
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+$string['access'] = 'އެކްސެސިބިލިޓީ';
+$string['accesshelp'] ='އެކްސެސިބިލިޓީ އެހީތެރިކަން';
+$string['accesskey'] = 'އެކްސެސް ކީ, {$a}';
+$string['accessstatement'] = 'އެކްސެސިބިލިޓީ ސްޓޭޓްމަންޓް';
+$string['activitynext'] = 'ދެން އޮތް ހަރަކާތް';
+$string['activityprev'] = 'ކުރީގެ ޙަރަކާތް';
+$string['breadcrumb'] = 'ނެވިގޭޝަން ބާރ';
+$string['eventcontextlocked'] = 'ކޮންޓެންޓް ހިފެހެއްޓިފައި';
+$string['eventcontextunlocked'] = 'ކޮންޓެންޓް ހުޅުވިފައި';
+$string['hideblocka'] = 'ގެ ބްލޮކް ފޮރުވާލާ{$a}';
+$string['showblocka'] = 'ގެ ބްލޮކް ދައްކާ{$a}';
+$string['sitemap'] = 'ސައިޓް މެޕް';
+$string['skipa'] = 'ސްކިޕް {$a}';
+$string['skipblock'] = 'ސްކިޕް ބްލޮކް';
+$string['skipnavigation'] = 'ސްކިޕް ނެވިގޭޝަން';
+$string['skipto'] = 'އަށް ސްކިޕްކޮށްލާ{$a}';
+$string['tocontent'] = 'މައިކޮންޓެންޓަށް ސްކިޕްކޮށްލާ';
+$string['tonavigation'] = 'ނެވިގޭޝަނަށް ދާށޭ';
+$string['youarehere'] = 'މިހާރު ތިހިރީ މިތަނުގައި';
+
